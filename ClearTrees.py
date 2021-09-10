44f1fb5f561704df0739a7519dcb95ed6469808f
b-ben-03 <s3906302@student.rmit.edu.au>
2021-09-10 11:19:05 +1000

    Add threading (crash when area_size large)

--- ClearTrees.py
+++ ClearTrees.py
@@ -1,13 +1,16 @@
 from mcpi.minecraft import Minecraft
 from mcpi import block as Block
 from vec3 import Vec
+from threading import Thread
 
 mc = Minecraft.create()
 
 air = Block.AIR.id
 
+threads = []
+
 # Size of area to clear
-area_size = Vec(10, 10, 10)
+area_size = Vec(30, 0, 30)
 
 # Player pos
 x, y, z = mc.player.getPos()
@@ -18,11 +21,15 @@ cy = int(y - area_size.y / 2)
 cz = int(z - area_size.z / 2)
 
 def CheckHighestBlock(x, z):
-    height = mc.getHeight(x, z)
+    new_mc = Minecraft.create()
+
+    height = new_mc.getHeight(x, z)
     DeleteTreeBlocksRecursive(x, height, z)
 
 def DeleteTreeBlocksRecursive(x, y, z):
-    curr_block = mc.getBlock(x, y, z)
+    new_mc = Minecraft.create()
+
+    curr_block = new_mc.getBlock(x, y, z)
 
     # Logs or leaves
     tree_cond = curr_block == 17 or curr_block == 18
@@ -44,11 +51,29 @@ print("Clearing Trees...")
 
 # super slow for large areas
 # TODO: Multithreading?
-def ClearTrees():
-    for ix in range(cx, cx + area_size.x):
-        for iz in range(cz, cz + area_size.z):
-            CheckHighestBlock(ix, iz) # waits until this is complete before moving to next block
+def ClearTrees(min_x, max_x, min_z, max_z):
+    for ix in range(min_x, max_x):
+        for iz in range(min_z, max_z):
+            thread = Thread(target=CheckHighestBlock, args=(ix, iz))
 
-ClearTrees()
+            thread.start()
+
+            if iz == max_z:
+                thread.join()
+
+ClearTrees(cx, cx + area_size.x, cz, cz + area_size.z)
+
+'''
+subdivisions = 4
+
+for x in range(subdivisions):
+    for z in range(subdivisions):
+        ClearTrees(
+            cx + area_size.x // subdivisions * (x - 1),
+            cx + area_size.x // subdivisions * x,
+            cz + area_size.z // subdivisions * (z - 1),
+            cz + area_size.z // subdivisions * z
+        )
+'''
 
 print("Trees cleared")
